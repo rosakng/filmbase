b3ab14820c5ae2a0a874b00d96e972e015b1f197
Rosa Kang <rosakang12@gmail.com>
2020-09-07 15:04:14 -0400

    WIP content filtering

--- filmbase-api/app.py
+++ filmbase-api/app.py
@@ -2,6 +2,8 @@ from chalice import Chalice
 import pandas as pd
 from sklearn.metrics.pairwise import sigmoid_kernel
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
 
 from utils.helper import get_serialized_films, get_weighted_rating
 
@@ -80,3 +82,40 @@ def get_trending_now():
     filtered_movies = filtered_movies.sort_values('weighted_average', ascending=False)
     filtered_movies[['original_title', 'vote_count', 'vote_average', 'weighted_average', 'popularity']].head(20)
     return str(filtered_movies.head(10))
+
+
+@app.route('/v1/filmbase/plot', methods=["GET"])
+def get_reccs_by_plot():
+    title = app.current_request.query_params["title"]
+
+    movies = get_serialized_films()
+    tfidf = TfidfVectorizer(stop_words='english')
+
+    # Replace NaN with an empty string
+    movies['overview'] = movies['overview'].fillna('')
+    tfidf_matrix = tfidf.fit_transform(movies['overview'])
+
+    # in (X,Y), Y is the number of different words that were use to describe X movies in the data
+    tfidf_matrix.shape
+
+    cosine_similarity = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+    # Create a reverse map of movie titles and DataFrame indices.
+    # This is so that we can find the index of a movie in our DataFrame, given its title.
+    indices = pd.Series(tfidf_matrix.index, index=tfidf_matrix['title']).drop_duplicates()
+
+    return get_recommendations_by_title(movies, title, indices, cosine_similarity)
+
+
+def get_recommendations_by_title(movies, title, indices, cosine_similarity):
+    index = indices[title]
+
+    similarity_scores = list(enumerate(cosine_similarity[index]))
+
+    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+
+    top_ten = similarity_scores[1:11]
+    movie_indices = [i[0] for i in top_ten]
+
+    return movies['title'].iloc[movie_indices]
+
