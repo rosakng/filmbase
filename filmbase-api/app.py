c5c203ca5c2262e250f8df90926f98a0b5ea1292
Rosa Kang <rosakang12@gmail.com>
2020-09-13 12:51:49 -0400

    Pearson Correlation function

--- filmbase-api/app.py
+++ filmbase-api/app.py
@@ -8,9 +8,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from surprise.model_selection import cross_validate
 from surprise import Reader, Dataset, SVD
 from ast import literal_eval
+from math import sqrt
 
 from utils.helper import get_film_data, get_recommendations_by_title, get_weighted_rating, get_director_name, \
-    get_top_elements, convert_data, create_metadata_soup
+    get_top_elements, convert_data, create_metadata_soup, get_clean_movies_dataframe, get_clean_ratings_dataframe
 
 app = Chalice(app_name='filmbase')
 
@@ -32,7 +33,8 @@ def get_trending_now():
 
     filtered_movies = filtered_movies.sort_values('weighted_average', ascending=False)
 
-    return str(filtered_movies[['original_title', 'vote_count', 'vote_average', 'weighted_average', 'popularity']].head(20))
+    return str(
+        filtered_movies[['original_title', 'vote_count', 'vote_average', 'weighted_average', 'popularity']].head(20))
 
 
 @app.route('/v1/filmbase/results/plot', methods=["GET"])
@@ -147,5 +149,108 @@ def get_reccs_by_ratings():
     return svd.predict(1, 100, 3)
 
 
+@app.route('/v1/filmbase/results/reccs', methods=["GET"])
+def get_reccs_by_user_input_ratings():
+    # This method uses Collaborative Filtering (User-User Filtering) to generate recommended items
+    # It attempts to find users that have similar preferences by using the Pearson Correlation Function
+    movies_df = get_clean_movies_dataframe()
+    ratings_df = get_clean_ratings_dataframe()
+
+    # TODO: Create post request for user input
+    userInput = [
+        {'title': 'Breakfast Club, The', 'rating': 5},
+        {'title': 'Toy Story', 'rating': 3.5},
+        {'title': 'Jumanji', 'rating': 2},
+        {'title': "Pulp Fiction", 'rating': 5},
+        {'title': 'Akira', 'rating': 4.5}
+    ]
+    # Generate dataframe for user input with rated movies
+    inputMovies = pd.DataFrame(userInput)
+    # print(inputMovies.head())
+
+    # Find rows that contain the rated movie titles, then merge it
+    # Merged dataframe will movieId, title, and rating of movies that were rated
+    inputId = movies_df[movies_df['title'].isin(inputMovies['title'].tolist())]
+    # TODO: some check to make sure that the rated movies are in the movie dataframe
+    inputMovies = pd.merge(inputId, inputMovies)
+    inputMovies = inputMovies.drop('year', 1)
+
+    # Use ratings dataframe and get users that have rated the same movies as the input
+    userSubset = ratings_df[ratings_df['movieId'].isin(inputMovies['movieId'].tolist())]
+    # print(userSubset.head())
+    userSubsetGroup = userSubset.groupby(['userId'])
+    # Group by a certain user id:
+    print(userSubsetGroup.get_group(200))
+    userSubsetGroup = sorted(userSubsetGroup, key=lambda x: len(x[1]), reverse=True)
+    print(userSubsetGroup[:3])
+
+    userSubsetGroup = userSubsetGroup[0:100]
+
+    # use Pearson Correlation Coefficient to find similarity of users to the input user
+    pearsonCorrelationDict = {}
+    # For every user group in our subset
+    for name, group in userSubsetGroup:
+        # Sorting the input and current user group
+        group = group.sort_values(by='movieId')
+        inputMovies = inputMovies.sort_values(by='movieId')
+        # N value for the formula
+        nRatings = len(group)
+        # Review scores for the movies that they both have in common
+        temp_df = inputMovies[inputMovies['movieId'].isin(group['movieId'].tolist())]
+        # And then store them in a temporary buffer variable in a list
+        tempRatingList = temp_df['rating'].tolist()
+        # Put the current user group reviews in a list format
+        tempGroupList = group['rating'].tolist()
+
+        # Calculate the pearson correlation between two users, so called, x and y
+        # See Sum of Squares formula
+        Sxx = sum([i ** 2 for i in tempRatingList]) - pow(sum(tempRatingList), 2) / float(nRatings)
+        Syy = sum([i ** 2 for i in tempGroupList]) - pow(sum(tempGroupList), 2) / float(nRatings)
+        Sxy = sum(i * j for i, j in zip(tempRatingList, tempGroupList)) - sum(tempRatingList) * sum(
+            tempGroupList) / float(nRatings)
+
+        # If the denominator is not zero, then divide, else, set correlation to 0
+        if Sxx != 0 and Syy != 0:
+            pearsonCorrelationDict[name] = Sxy / sqrt(Sxx * Syy)
+        else:
+            pearsonCorrelationDict[name] = 0
+
+        print(pearsonCorrelationDict.items())
+
+        pearsonDF = pd.DataFrame.from_dict(pearsonCorrelationDict, orient='index')
+        pearsonDF.columns = ['similarityIndex']
+        pearsonDF['userId'] = pearsonDF.index
+        pearsonDF.index = range(len(pearsonDF))
+        print(pearsonDF.head())
+
+        # Change this number to choose top X about of similar users
+        topUsers = pearsonDF.sort_values(by='similarityIndex', ascending=False)[0:60]
+        print(topUsers.head())
+
+        topUsersRating = topUsers.merge(ratings_df, left_on='userId', right_on='userId', how='inner')
+        print(topUsersRating.head())
+
+        # Multiplies the similarity by the user's ratings
+        topUsersRating['weightedRating'] = topUsersRating['similarityIndex'] * topUsersRating['rating']
+        print(topUsersRating.head())
+
+        # Applies a sum to the topUsers after grouping it up by userId
+        tempTopUsersRating = topUsersRating.groupby('movieId').sum()[['similarityIndex', 'weightedRating']]
+        tempTopUsersRating.columns = ['sum_similarityIndex', 'sum_weightedRating']
+        print(tempTopUsersRating.head())
+
+        # Creates an empty dataframe
+        recommendation_df = pd.DataFrame()
+        # Now we take the weighted average
+        recommendation_df['weighted average recommendation score'] = tempTopUsersRating['sum_weightedRating'] / tempTopUsersRating['sum_similarityIndex']
+        recommendation_df['movieId'] = tempTopUsersRating.index
+        print(recommendation_df.head())
+
+        recommendation_df = recommendation_df.sort_values(by='weighted average recommendation score', ascending=False)
+        print(recommendation_df.head(10))
+
+        return str(movies_df.loc[movies_df['movieId'].isin(recommendation_df.head(10)['movieId'].tolist())])
+
+
 if __name__ == '__main__':
-    get_reccs_by_ratings()
+    get_reccs_by_user_input_ratings()
